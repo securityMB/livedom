2f5d66ef46549b0ac1c60cc66f88e9ffa308cc1f
Michał Bentkowski <michal@bentkowski.info>
2022-12-22 17:42:35 +0100

    Add verification for Sanitizer API.

--- src/parsers/07sanitizer-api.js
+++ src/parsers/07sanitizer-api.js
@@ -1,10 +1,15 @@
 // name: Sanitizer API
 class ClosureSanitizer {
   init() {
+    this.available = typeof Element.prototype.setHTML === "function";
     this.div = document.createElement("div");
   }
 
   parse(s) {
+    if (!this.available) {
+      display("Your browser does not support Sanitizer API");
+      return;
+    }
     this.div.setHTML(s);
     display(this.div);
   }
